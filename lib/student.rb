a17018ccacc90a36f04723d3b2ecb172af8deac1
erikn775 <erikn775@gmail.com>
2020-11-05 19:08:23 -0700

    commit tree da1b43c8ff4df90557627f3e2610de6ee2212807

--- lib/student.rb
+++ lib/student.rb
@@ -1,3 +1,15 @@
-class Student 
+class Student < User
+
+    def initialize
+        @knowledge = []
+    end
+
+    def learn(truthiness_of_the_teachings)
+        @knowledge.push(truthiness_of_the_teachings)
+    end
+
+    def knowledge
+        @knowledge
+    end
 
 end
